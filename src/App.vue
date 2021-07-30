36c69f37ed57d2beee003c37245c5c2ff4f57081
馒头 <liusx@missfresh.cn>
2021-07-30 20:32:08 +0800

    feat: 增加对腾讯地图的隔离

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,38 @@
 <template>
-  <layout/>
+  <layout />
 </template>
 
 <script>
+import { register, startQK } from './qiankun/qiankun';
 
-import { register, startQK } from './qiankun/qiankun'
+import HelloWorld from './components/HelloWorld.vue';
+import Layout from '@/components/Layout/Layout.vue';
 
-import HelloWorld from './components/HelloWorld.vue'
-import Layout from '@/components/Layout/Layout.vue'
-
-import { ref, reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted } from 'vue';
 
 export default {
   name: 'App',
   setup() {
-    onMounted(()=> {
-      console.log('register', register)
+    onMounted(() => {
+      console.log('register', register);
       register();
       startQK({
         prefetch: 'all',
         sandbox: {
-          strictStyleIsolation: true,
+          strictStyleIsolation: true
           // experimentalStyleIsolation: true,
         },
-        singular: true
-      })
-    })
-    return { }
-   },
+        singular: true,
+        excludeAssetFilter: (assetUrl) => {
+          return /(?=map.qq.com)/.test(assetUrl);
+        }
+      });
+    });
+    return {};
+  },
   components: {
     HelloWorld,
     Layout
-  },
-  
-}
+  }
+};
 </script>
